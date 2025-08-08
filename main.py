abd2dafcd011843c81ad19741ca24272d78e3ca2
Mobin <mobinking18@yahoo.com>
2025-08-08 14:52:58 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 import re
+import os
 from handlers.link_handler import handle_link
 
-# توکن مستقیم داخل کد
+API_ID = 20292726
+API_HASH = "86902140c904c0de4a5813813c9a2409"
 BOT_TOKEN = "8431602847:AAGPz5QpBiwfVdi-2XPKif9abqxl_Uq7Cow"
 
-# استفاده از session موقتی تا Pyrogram دنبال user session قدیمی نگرده
 app = Client(
-    name=":memory:",  # بدون ذخیره‌سازی session در فایل
+    "auto_downloader_bot",
+    api_id=API_ID,
+    api_hash=API_HASH,
     bot_token=BOT_TOKEN
 )
 
-@app.on_message((filters.private | filters.group) & filters.text)
+@app.on_message(filters.private | filters.group & filters.text)
 async def on_message(client: Client, message: Message):
     urls = re.findall(r'(https?://[^\s]+)', message.text or "")
     if not urls:
@@ -32,3 +35,4 @@ async def start_handler(client: Client, message: Message):
 
 if __name__ == "__main__":
     app.run()
+    
