a73caeb0eff39cad7224b5877e11fcb24ade148e
Mobin <mobinking18@yahoo.com>
2025-08-09 13:46:28 +0330

    Update mediafire.py

--- services/mediafire.py
+++ services/mediafire.py
@@ -1,11 +1,22 @@
 import requests
+import tempfile
+import os
+import re
 
-def download_mediafire(url: str, save_path: str):
-    # فرض بر این است که URL به یک فایل مستقیم اشاره می‌کنه.
+async def download_mediafire(url):
     try:
-        response = requests.get(url)
-        with open(save_path, 'wb') as file:
-            file.write(response.content)
-        return f"Download successful! File saved to {save_path}"
-    except Exception as e:
-        return f"Error occurred: {e}"
+        page = requests.get(url).text
+        dl_url = re.search(r'href="(https://download[^"]+)"', page).group(1)
+
+        temp_dir = tempfile.mkdtemp()
+        local_path = os.path.join(temp_dir, dl_url.split("/")[-1])
+
+        with requests.get(dl_url, stream=True) as r:
+            r.raise_for_status()
+            with open(local_path, "wb") as f:
+                for chunk in r.iter_content(chunk_size=8192):
+                    f.write(chunk)
+
+        return local_path
+    except:
+        return None
